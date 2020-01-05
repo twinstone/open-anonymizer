7c9f9d66893121d3846405bce231dd6ffe0f3b14
alexchmel <sashachmelok@gmail.com>
2020-01-05 15:55:12 +0100

    Build script and test

---
+++ src/test/java/application/ConfigurationLoaderTest.java
@@ -0,0 +1,29 @@
+package application;
+
+import application.config.Configuration;
+import application.config.loader.ConfigurationLoader;
+import application.config.loader.JsonConfigurationLoader;
+import application.core.AnonymizationService;
+import application.datasource.DataSource;
+import application.model.wrapper.DataSet;
+import application.model.wrapper.EntityWrapper;
+import org.junit.Test;
+
+public class ConfigurationLoaderTest {
+
+    private final static String PATH = "F:/Bachelor/src/main/resources/config.json";
+
+    @Test
+    public void test() {
+        ConfigurationLoader loader = new JsonConfigurationLoader();
+        Configuration configuration = loader.readConfiguration(PATH);
+        AnonymizationService service = new AnonymizationService();
+        DataSource dataSource = configuration.getInputSource();
+        DataSet dataSet = dataSource.readDataSet(configuration.getEntities().get(0));
+        while (dataSet.hasNext()) {
+            EntityWrapper wrapper = dataSet.next();
+            service.anonymizeEntity(wrapper, configuration.getEntities().get(0));
+        }
+    }
+
+}

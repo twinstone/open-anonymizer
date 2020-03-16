97647c410c22c06a38f72a4294a680e7fddec50b
alexchmel <sashachmelok@gmail.com>
2020-03-16 07:40:23 +0100

    Aliases for builders, ignoring test configs.

--- src/main/java/application/config/Aliases.java
+++ src/main/java/application/config/Aliases.java
@@ -11,8 +11,21 @@ public class Aliases {
         put("FIRST_NAME", "application.core.anonymizer.FirstNameAnonymizer");
     }});
 
+    private final static Map<String, String> BUILDERS_ALIAS = Collections.unmodifiableMap(new HashMap<String, String>() {{
+        put("MONGO", "application.datasource.mongo.MongoBuilder");
+        put("MYSQL", "application.datasource.mysql.MySqlBuilder");
+        put("CSV", "application.datasource.csv.CsvBuilder");
+    }});
+
     public static String tryFind(final String key) {
         return null;
+    }
+
+    public static String tryFindBuilderClass(String name) {
+        if (BUILDERS_ALIAS.containsKey(name)) {
+            return BUILDERS_ALIAS.get(name);
+        }
+        return name;
     }
 
     public static String tryFindAnonymizationClass(String name) {
